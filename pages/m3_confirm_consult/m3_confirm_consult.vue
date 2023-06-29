d93e690a4f923b4b07115d786deede2b7b3cf300
无糖可乐加冰 <109839698+lazydog-pp@users.noreply.github.com>
2023-06-30 02:00:41 +0800

    feat: 增加M3模块页面设计

---
+++ pages/m3_confirm_consult/m3_confirm_consult.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		<view>
+			<uni-card>
+				<text class="title">{{name}}</text>
+			</uni-card>
+			<uni-card>
+				<text class="title">{{name}}</text>
+			</uni-card>
+			<uni-card>
+				<text class="title">{{name}}</text>
+			</uni-card>
+			<uni-card>
+				<text class="title">{{name}}</text>
+			</uni-card>
+			<uni-card>
+				<text class="title">{{name}}</text>
+			</uni-card>
+			<uni-card>
+				<text class="title">{{name}}</text>
+			</uni-card>
+		</view>		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				name:'pp'
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.tltle{
+		
+	}
+</style>
