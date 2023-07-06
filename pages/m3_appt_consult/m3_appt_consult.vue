694f85d38b7992af0edd0892d3e5cb901b5720a5
无糖可乐加冰 <109839698+lazydog-pp@users.noreply.github.com>
2023-07-06 21:56:03 +0800

    feat: M3模块数据库

--- pages/m3_appt_consult/m3_appt_consult.vue
+++ pages/m3_appt_consult/m3_appt_consult.vue
@@ -2,26 +2,26 @@
 	<view>
 		<view class="appt_content">
 			
-			<uni-section :title="'导师姓名：'+teacher_name" type="line"></uni-section>
+			<uni-section :title="'导师姓名：'+consult.teach_name" type="line"></uni-section>
 			
 			<uni-section title="咨询主题:" class="label" type="line"></uni-section>
 			<view class="appt_theme">
 				<uni-data-select 
-				v-model="appt_label" 
+				v-model="consult.appt_label" 
 				:localdata="appt_theme" 
 				:clear="false" 
 				@change="apptpick_theme()">
 				</uni-data-select>
 			</view>
 			
-			<uni-section :title="'咨询价格：'+appt_cost+'元/30min'" type="line"></uni-section>
+			<uni-section :title="'咨询价格：'+consult.appt_price+'元/30min'" type="line"></uni-section>
 			
 			<view class="appt_duration">
 				<view>
-					<uni-section :title="'预约时长 : '+ last_duration+'分钟'" type="line"></uni-section>
+					<uni-section :title="'预约时长 : '+consult.appt_duration+'分钟'" type="line"></uni-section>
 				</view>
 				<view>
-					<uni-number-box :step=30 :max=120 :min=0 :value="last_duration" @change="duration_change" background="#2979FF" color="#fff" />
+					<uni-number-box :step=30 :max=120 :min=0 :value="consult.appt_duration" @change="duration_change" background="#2979FF" color="#fff" />
 				</view>
 				<view class="extra"></view>
 				
@@ -30,16 +30,16 @@
 			
 			<uni-section :title="'预约日期:'" type="line"></uni-section>
 			<view class="appt_date">
-				<uni-datetime-picker v-model="getdate" type="date" :start="start" :clear-icon="false" :end="end" @maskClick="date_change" />
+				<uni-datetime-picker v-model="consult.appt_date" type="date" :start="start" :clear-icon="false" :end="end" @maskClick="date_change" />
 			</view>
 			
 			<uni-section :title="'开始时间:'" type="line" subTitle="可接受的咨询时间(多选)">
 				<view class="uni-px-5 uni-pb-5">
-					<uni-data-checkbox mode="tag" multiple v-model="apptpick_time" :localdata="Times"></uni-data-checkbox>
+					<uni-data-checkbox mode="tag" multiple v-model="consult.appt_time1" :localdata="Times"></uni-data-checkbox>
 				</view>
 			</uni-section>
 				
-			<uni-section :title="'支付价格：'+apply_cost+' 元'" type="line"></uni-section>
+			<uni-section :title="'支付价格：'+consult.appt_cost+' 元'" type="line"></uni-section>
 			<text>\n</text>
 			
 			<view class="confirm">
@@ -58,9 +58,6 @@
 	export default {
 		data() {
 			return {
-				teacher_name:'pp',
-				
-				appt_label: 0,
 				appt_theme:[
 					{ value: 0, text: '简历优化' },
 					{ value: 1, text: '面试经验' },
@@ -70,19 +67,10 @@
 					{ value: 5, text: '其他' }
 				],
 				
-				appt_cost:20,
-				apply_cost:0,
-				index: 0,
-				last_duration: 0,
-				appt_label: 0,
-				
-				getdate:Date.now(),
 				start:Date.now(),
 				end:Date.now()+14 * 24 * 3600000,
-				
 				gethours: new Date().getHours()+1,
 				
-				apptpick_time:[],
 				Times: [
 					{text: '08:00', value: 8}, 
 					{text: '09:00', value: 9},
@@ -101,6 +89,21 @@
 					{text: '22:00', value: 22},
 					{text: '23:00', value: 23}
 					],
+					
+				consult:{
+					"teach_tele": 12345678910,
+					"stud_tele": 12345678911,
+					"teach_name": "小王",
+					"stud_name": "小张",
+					"appt_label": 0,
+					"appt_price": 30,
+					"appt_duration": 0,
+					"appt_date": Date.now(),
+					"appt_time1": [],
+					"appt_time2": 23,
+					"appt_cost": 0,
+					"appt_state": 0
+				}
 			};
 		},
 		
@@ -112,14 +115,14 @@
 				console.log('maskClick事件:', e);
 			},
 			duration_change(value) {
-				this.last_duration = value;
-				this.apply_cost=value * this.appt_cost / 30;
+				this.consult.appt_duration = value;
+				this.consult.appt_cost=value * this.consult.appt_duration / 30;
 			},
 			checktime(pick) {
 				return pick >= this.gethours;
 			},
 			appointAndpay() {
-				if (this.last_duration==0) {
+				if (this.consult.appt_duration==0) {
 					uni.showToast({
 						title: '预约时长需大于零',
 						icon: 'none',
@@ -127,7 +130,7 @@
 					});
 					return;
 				}
-				if (Object.keys(this.apptpick_time).length==0) {
+				if (Object.keys(this.consult.appt_time1).length==0) {
 					uni.showToast({
 						title: '请选择开始时间',
 						icon: 'none',
@@ -135,8 +138,7 @@
 					});
 					return;
 				}
-				console.log('cuowu')
-				if (this.apptpick_time.slice(0,1)< this.gethours) {
+				if (this.consult.appt_time1.slice(0,1)< this.gethours) {
 					uni.showToast({
 						title: '请选择正确时间',
 						icon: 'none',
@@ -144,6 +146,12 @@
 					});
 					return;
 				}
+				
+				const db = uniCloud.database();
+				db.collection("consult").add(this.consult).then(e=>{
+					console.log(e)
+				})			   
+	   
 				uni.showToast({
 					title: '预约成功',
 					icon: 'none',
@@ -152,9 +160,10 @@
 				uni.switchTab({
 				  url: '../find_teacher/find_teacher'
 				});
-				
 			}
 		},
+		onLoad() {
+		}
 		
 	}
 </script>
